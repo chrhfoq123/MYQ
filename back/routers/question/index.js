7e1fd8b78fecfcb2b37fbde5ab0b3afc212635b2
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-08-19 23:19:15 +0900

    22-08-19 question insert api

--- back/routers/question/index.js
+++ back/routers/question/index.js
@@ -8,8 +8,26 @@ router.get("/", (req,res) => {
 
 // 문제 등록 API
 router.post("/", async (req,res) => {
-    //let query = `INSERT INTO question (subject, make_time) VALUES ("${req.body.subject}", now())`;    
-    res.send("SDFDSF");
+    let query = `INSERT INTO question (subject, make_time) VALUES ("${req.body.subject}", now())`;
+    const conn = await require("../../database")();
+    conn.query(query, (err,rows) => {
+        if(err) console.log(err);
+        const { insertId } = rows;
+        let sub_query = "INSERT INTO answer (subject, isAnswer, pkey) VALUES ";
+        for(let i=0; i<req.body.answers.length; i++)
+        {
+            const obj = req.body.answers[i];
+            sub_query += `("${obj.subject}", ${obj.isAnswer}, ${insertId})`;
+            if(i+1 < req.body.answers.length)
+            {
+                sub_query += ", ";
+            }
+        }
+        conn.query(sub_query, (_err, _rows) => {
+            if(_err) console.log(_err);
+            res.send(_rows);
+        });        
+    })    
 });
 
 module.exports = router;
