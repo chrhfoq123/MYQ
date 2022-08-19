33615d4f6eae3b0d21bbf0917b72454ab7e90df6
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-08-19 21:40:39 +0900

    22-08-19 add body-Parser

--- back/routers/question/index.js
+++ back/routers/question/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const conn = require('../../database');
-router.get("/", (req,res) => {
-    
+
+// 문제 List return API
+router.get("/", (req,res) => {    
     res.send("문제 페이지");
+});
+
+// 문제 등록 API
+router.post("/", (req,res)=>{    
+    res.send("SDFDSF");
 });
 
 module.exports = router;
