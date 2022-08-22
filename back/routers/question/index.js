f214cbccb971daa99bc514ae0614312121c43a71
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-08-22 20:07:56 +0900

    22-08-22 question get api

--- back/routers/question/index.js
+++ back/routers/question/index.js
@@ -38,10 +38,39 @@ router.post("/", async (req,res) => {
 });
 
 // 데이터 하나 가져오기
-router.get("/:idx", (req, res) => {
+router.get("/:idx", async (req, res) => {
     const { idx } = req.params;
-    console.log(idx);
-    res.send("AAAAAA");
+    let query = `
+    SELECT question.idx as qidx, question.subject as qsubject, make_time, answer.idx as aidx, answer.subject as asubject, answer.isAnswer
+    FROM question JOIN answer ON question.idx = answer.pkey WHERE question.idx = ${idx}
+    `;
+    const conn = await require("../../database")();
+    conn.query(query, (err,row) => {        
+        if(row.length <= 0)
+        {
+            res.send({msg : "No Data"});
+            return;
+        }
+
+        if(err) console.log(err);
+        console.log(row[0]);
+        const data = {
+            qidx : row[0].qidx,
+            subject : row[0].qsubject,
+            make_time : row[0].make_time,
+            answers : []
+        }
+        for(let i=0; i<row.length; i++)
+        {
+            const aobj = {
+                idx : row[i].aidx,
+                subject : row[i].asubject,
+                isAnswer : row[i].isAnswer
+            };
+            data.answers[i] = aobj;
+        }
+        res.send(data);
+    });
 });
 
 module.exports = router;
