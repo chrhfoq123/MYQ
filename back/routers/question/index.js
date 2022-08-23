3cc471baf6eb73e33455a0456b30f022a439590d
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-08-23 16:51:25 +0900

    22-08-23 add modify api

--- back/routers/question/index.js
+++ back/routers/question/index.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const dao = require('../DAO');
 
 // 문제 List return API
+// GET http://localhost:5000/question
+// 쿼리스트링 count = 가져올 리스트의 갯수
 router.get("/", async (req,res) => {        
     let count = req.query.count ? req.query.count : 15;
     let query = `SELECT * FROM question ORDER BY idx DESC LIMIT ${count}`;
-    console.log(query);
     const conn = await require('../../database')();
     conn.query(query, (err,row) => {
         if(err) console.log(err);
@@ -41,7 +42,7 @@ router.post("/", async (req,res) => {
 router.get("/:idx", async (req, res) => {
     const { idx } = req.params;
     let query = `
-    SELECT question.idx as qidx, question.subject as qsubject, make_time, answer.idx as aidx, answer.subject as asubject, answer.isAnswer
+    SELECT question.idx as qidx, question.subject as qsubject, make_time, answer.idx as aidx, answer.subject as asubject, answer.isAnswer, question.modify_time
     FROM question JOIN answer ON question.idx = answer.pkey WHERE question.idx = ${idx}
     `;
     const conn = await require("../../database")();
@@ -70,6 +71,31 @@ router.get("/:idx", async (req, res) => {
             data.answers[i] = aobj;
         }
         res.send(data);
+    });
+});
+
+// 데이터 수정 API
+// PATCH http://localhost:5000/:idx
+// -d "subject=modify_subject"
+router.patch("/:idx", async (req,res) => {
+    const idx = req.params.idx;
+    if(!idx || idx === undefined)
+    {
+        res.send({msg:"idx null"});
+        return;
+    }
+    const { subject } = req.body;    
+    let query = `UPDATE question SET `;
+    query += `modify_time = now(), `;
+    if(subject) {
+        query += `subject = "${subject}" `;
+    }
+    query += `WHERE idx = ${idx}`;
+    const conn = await require("../../database")();
+    conn.query(query, (err, row) => {
+        if(err) console.log(err);
+        row.idx = idx;
+        res.send(row);
     });
 });
 
