43a190446d2f1aab564bc345654355761cde7903
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-08-28 15:40:57 +0900

    22-08-28 문제집등록 시작

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,7 @@ import Question from './components/Question';
 import Main from './components/Main';
 import Header from './components/Header';
 import Book from './components/Book';
+import BookWrite from './components/book/BookWrite';
 import QuestionList from './components/QuestionList';
 import Developer from './components/Developer';
 
@@ -19,6 +20,7 @@ function App(props) {
             <Route path='Main' element={<Main/>}/>
             <Route path='Question' element={<Question/>}/>
             <Route path='Book' element={<Book/>}/>
+            <Route path='BookWrite' element={<BookWrite/>}/>
             <Route path='QuestionList' element={<QuestionList/>}/>
             <Route path='developer' element={<Developer/>}/>
           </Routes>
---
+++ front/src/components/book/BookWrite.js
@@ -0,0 +1,7 @@
+function BookWrite()
+{
+    return(
+        <div>대충 문제집을 등록하는 페이지</div>
+    );
+}
+export default BookWrite;
