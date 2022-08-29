a03fefad2f5f49c909d1711c74df64aceda98413
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-08-29 16:44:31 +0900

    22-08-29 add bookView

---
+++ front/src/components/book/BookView.js
@@ -0,0 +1,15 @@
+import { Form, Button, Table, Modal } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import Question from '../Question';
+import useFetch from '../../hook/useFetch';
+import axios from 'axios';
+function BookView()
+{
+    return(                
+        <div className='book-view'>
+            test
+        </div>
+    );
+
+}
+export default BookView;
