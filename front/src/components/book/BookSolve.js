861d1fe8a464c5a5be799fb138a969f1755fac85
chrhfoq123 <chrhfoq123@naver.com>
2022-09-04 23:18:50 +0900

    버튼 효과 추가

--- front/src/components/book/BookSolve.js
+++ front/src/components/book/BookSolve.js
@@ -13,27 +13,27 @@ function BookSolve()
                 </div>
                 <div className='booksolve-answer'>
                     <div className='answer-check'>
-                        <Button variant='outline-secondary' onClick={()=> {setIsAnswer(1)}}>1</Button>{' '}
+                        <Button variant={isAnswer?'secondary':'outline-secondary'} onClick={()=> {setIsAnswer(1)}}>1</Button>{' '}
                         <span>일번답</span>
                     </div>
                     <div className='answer-check'>
-                        <Button variant='outline-secondary'>2</Button>{' '}
+                        <Button variant={isAnswer?'secondary':'outline-secondary'} onClick={()=> {setIsAnswer(1)}}>2</Button>{' '}
                         <span>이번답</span>
                     </div>
                     <div className='answer-check'>
-                        <Button variant={/*여기 버튼 색깔바뀌게*/outline-secondary}>3</Button>{' '}
+                        <Button variant={isAnswer?'secondary':'outline-secondary'} onClick={()=> {setIsAnswer(1)}}>3</Button>{' '}
                         <span>삼번답</span>
                     </div>
                     <div className='answer-check'>
-                        <Button variant='outline-secondary'>4</Button>{' '}
+                        <Button variant={isAnswer?'secondary':'outline-secondary'} onClick={()=> {setIsAnswer(1)}}>4</Button>{' '}
                         <span>시번답</span>
                     </div>
                     <div className='answer-check'>
-                        <Button variant='outline-secondary'>5</Button>{' '}
+                        <Button variant={isAnswer?'secondary':'outline-secondary'} onClick={()=> {setIsAnswer(1)}}>5</Button>{' '}
                         <span>오번답</span>
                     </div>
                     <div className='answer-submit'>
-                        <Button variant='secondary'>제출</Button>
+                        <Button variant='secondary' size='lg'>제출</Button>
                     </div>
                 </div>
             </div>
