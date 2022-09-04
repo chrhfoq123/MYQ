623816caddac3be4cfafdc9f295d5beb3b32649b
chrhfoq123 <chrhfoq123@naver.com>
2022-09-04 22:22:23 +0900

    문제풀기 ui 구현

--- front/src/components/book/BookSolve.js
+++ front/src/components/book/BookSolve.js
@@ -1,10 +1,41 @@
+import {Button} from 'react-bootstrap';
+import { useState } from 'react';
 
 function BookSolve()
 {
+    const[isAnswer, setIsAnswer] = useState(0);
+
     return(
         <div className='booksolve-main'>
             <div className='booksolve-area'>
-                <span>문제문제문제문제문제문제문제문제문제문제문제문제문제문제</span>
+                <div className='booksolve-question'>
+                    <span>1. 문제문제문제문제문제문제문제문제문제문제문제문제문제문제</span>
+                </div>
+                <div className='booksolve-answer'>
+                    <div className='answer-check'>
+                        <Button variant='outline-secondary' onClick={()=> {setIsAnswer(1)}}>1</Button>{' '}
+                        <span>일번답</span>
+                    </div>
+                    <div className='answer-check'>
+                        <Button variant='outline-secondary'>2</Button>{' '}
+                        <span>이번답</span>
+                    </div>
+                    <div className='answer-check'>
+                        <Button variant={/*여기 버튼 색깔바뀌게*/outline-secondary}>3</Button>{' '}
+                        <span>삼번답</span>
+                    </div>
+                    <div className='answer-check'>
+                        <Button variant='outline-secondary'>4</Button>{' '}
+                        <span>시번답</span>
+                    </div>
+                    <div className='answer-check'>
+                        <Button variant='outline-secondary'>5</Button>{' '}
+                        <span>오번답</span>
+                    </div>
+                    <div className='answer-submit'>
+                        <Button variant='secondary'>제출</Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
